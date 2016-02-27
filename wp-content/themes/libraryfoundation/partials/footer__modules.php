589670b580aa8ae15d61752fa66e72999ae0ca79
Craig Fowler <craig@wearegiants.com>
2016-02-26 18:23:58 -0800

    fotoer changes

--- wp-content/themes/libraryfoundation/partials/footer__modules.php
+++ wp-content/themes/libraryfoundation/partials/footer__modules.php
@@ -15,11 +15,11 @@
 <hr class="invisible fs-lg-hide">
 
 <div class="fs-cell fs-lg-half fs-md-full fs-sm-3 equalize">
-<div class="fs-cell fs-xl-half fs-lg-5 fs-md-2 fs-sm-1 fs-contained footer__module-wrapper footer__module-bg" style="background-image:url('/wp-content/uploads/maguire-gardens-600x335.jpg')"></div>
+<div class="fs-cell fs-xl-half fs-lg-5 fs-md-2 fs-sm-1 fs-contained footer__module-wrapper footer__module-bg" style="background-image:url('/wp-content/uploads/about-600x335.jpg')"></div>
 <div class="fs-cell fs-xl-half fs-lg-7 fs-md-4 fs-sm-2 fs-contained footer__module-wrapper bg__color-bgGray">
   <div class="wrapper">
-  <h3 class="footer__module-title title__sm">Become a Member</h3>
-  <div class="fs-sm-hide">Join a community of Los Angeles Public Library supporters.</div>
+  <h3 class="footer__module-title title__sm">About Us</h3>
+  <div class="fs-sm-hide">Learn more about the Library Foundation.</div>
   <a class="btn color__blue btn__sm btn__rounded btn__outlined fs-sm-hide" href="#">Learn More</a>
   </div>
 </div>
@@ -28,11 +28,11 @@
 <hr class="invisible">
 
 <div class="fs-cell fs-lg-half fs-md-full fs-sm-3 equalize">
-<div class="fs-cell fs-xl-half fs-lg-5 fs-md-2 fs-sm-1 fs-contained footer__module-wrapper footer__module-bg" style="background-image:url('/wp-content/uploads/maguire-gardens-600x335.jpg')"></div>
+<div class="fs-cell fs-xl-half fs-lg-5 fs-md-2 fs-sm-1 fs-contained footer__module-wrapper footer__module-bg" style="background-image:url('/wp-content/uploads/1227_IMG_4956-600x335.jpg')"></div>
 <div class="fs-cell fs-xl-half fs-lg-7 fs-md-4 fs-sm-2 fs-contained footer__module-wrapper bg__color-bgGray">
   <div class="wrapper">
-  <h3 class="footer__module-title title__sm">Become a Member</h3>
-  <div class="fs-sm-hide">Join a community of Los Angeles Public Library supporters.</div>
+  <h3 class="footer__module-title title__sm">The Library Store</h3>
+  <div class="fs-sm-hide">Shop our collection of literary gifts and goods.</div>
   <a class="btn color__blue btn__sm btn__rounded btn__outlined fs-sm-hide" href="#">Learn More</a>
   </div>
 </div>
@@ -41,11 +41,11 @@
 <hr class="invisible fs-lg-hide">
 
 <div class="fs-cell fs-lg-half fs-md-full fs-sm-3 equalize">
-<div class="fs-cell fs-xl-half fs-lg-5 fs-md-2 fs-sm-1 fs-contained footer__module-wrapper footer__module-bg" style="background-image:url('/wp-content/uploads/maguire-gardens-600x335.jpg')"></div>
+<div class="fs-cell fs-xl-half fs-lg-5 fs-md-2 fs-sm-1 fs-contained footer__module-wrapper footer__module-bg" style="background-image:url('/wp-content/uploads/Student-Zones1-600x335.jpg')"></div>
 <div class="fs-cell fs-xl-half fs-lg-7 fs-md-4 fs-sm-2 fs-contained footer__module-wrapper bg__color-bgGray">
   <div class="wrapper">
-  <h3 class="footer__module-title title__sm">Become a Member</h3>
-  <div class="fs-sm-hide">Join a community of Los Angeles Public Library supporters.</div>
+  <h3 class="footer__module-title title__sm">What We Fund</h3>
+  <div class="fs-sm-hide">Enriching the programs of the Los Angeles Public Library.</div>
   <a class="btn color__blue btn__sm btn__rounded btn__outlined fs-sm-hide" href="#">Learn More</a>
   </div>
 </div>
