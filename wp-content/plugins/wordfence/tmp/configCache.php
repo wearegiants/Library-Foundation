ee8e2aa1d2af06616f41d39ab6a539945c13c68d
Craig Fowler <craig@wearegiants.com>
2016-04-18 10:40:58 -0700

    updated woocommerce checkout manager

--- wp-content/plugins/wordfence/tmp/configCache.php
+++
@@ -1,4 +0,0 @@
-<?php
-/* Wordfence temporary file security header */
-echo "Nothing to see here!\n"; exit(0);
-?>a:16:{s:9:"cacheType";s:0:"";s:9:"howGetIPs";s:0:"";s:11:"whitelisted";s:0:"";s:6:"apiKey";s:128:"231d4fa259bea95988be06468385311346f33eacee5aa2e433121d6adb9e82b02c1071e243e4dcebf58d20c64dba9cde898913e388bdaf2bd1a1a4e73e9a60b6";s:20:"loginSec_lockoutMins";s:3:"240";s:19:"other_hideWPVersion";s:1:"1";s:15:"firewallEnabled";s:1:"1";s:18:"liveTrafficEnabled";s:1:"1";s:14:"disableCookies";s:1:"0";s:17:"actUpdateInterval";s:0:"";s:7:"debugOn";s:1:"0";s:10:"tourClosed";s:0:"";s:13:"welcomeClosed";s:1:"1";s:11:"other_WFNet";s:1:"1";s:26:"loginSec_disableAuthorScan";s:1:"1";s:25:"liveTraf_ignorePublishers";s:1:"1";}
